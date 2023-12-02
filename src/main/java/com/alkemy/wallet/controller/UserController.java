3e33623096ac393f68b8effa9a81adb2d1e93325
sebastian <sebastiandev.img@gmail.com>
2023-12-01 23:50:07 -0300

    T8S2-149:endpoint para listar usuarios

--- src/main/java/com/alkemy/wallet/controller/UserController.java
+++ src/main/java/com/alkemy/wallet/controller/UserController.java
@@ -5,10 +5,9 @@ import com.alkemy.wallet.service.IUserService;
 import com.alkemy.wallet.service.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -18,6 +17,13 @@ public class UserController {
 
     public UserController(UserServiceImpl userService){
         this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(){
+        //TODO: devolver un dto de users?
+        List<User> users = userService.getUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
 
--- src/main/java/com/alkemy/wallet/service/IUserService.java
+++ src/main/java/com/alkemy/wallet/service/IUserService.java
@@ -2,6 +2,9 @@ package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.entity.User;
 
+import java.util.List;
+
 public interface IUserService {
+    List<User> getUsers();
     User deleteUserById(Long id);
 }
