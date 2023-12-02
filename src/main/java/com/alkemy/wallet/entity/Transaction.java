e27900e4735e77951a2a8a082e7c26eaa107d7f9
sebastian <sebastiandev.img@gmail.com>
2023-12-01 23:02:50 -0300

    T8S2-143:se creo entidad y repositorio de transaction

---
+++ src/main/java/com/alkemy/wallet/entity/Transaction.java
@@ -0,0 +1,42 @@
+package com.alkemy.wallet.entity;
+
+import com.alkemy.wallet.enums.ETransaction;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
+    private long id;
+
+    @Column(name = "AMOUNT", nullable = false)
+    private double amount;
+
+    @Column(name = "TYPE", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ETransaction type;
+
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "TRANSACTION_DATE", nullable = false)
+    private Timestamp transactionDate;
+
+    @ManyToOne
+    @JoinColumn(name="ACCOUNT_ID", referencedColumnName = "ID")
+    private Account account;
+
+}
---
+++ src/main/java/com/alkemy/wallet/repository/ITransactionRepository.java
@@ -0,0 +1,10 @@
+package com.alkemy.wallet.repository;
+
+import com.alkemy.wallet.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
