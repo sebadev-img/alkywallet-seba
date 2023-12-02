5fadb14656eff6f24fe2ab2d1fb6b7ce0517af56
sebastian <sebastiandev.img@gmail.com>
2023-12-01 23:13:57 -0300

    hotfix:se arreglo errores de importacion en entidades

--- src/main/java/com/alkemy/wallet/entity/User.java
+++ src/main/java/com/alkemy/wallet/entity/User.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
