e50e3b14038fa25e3f5797f14babbba1c4a252fa
sebastian <sebastiandev.img@gmail.com>
2023-12-08 12:40:22 -0300

    T8S2-162:endpoint para obtener una transaccion por id

--- src/main/java/com/alkemy/wallet/service/TransactionServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/TransactionServiceImpl.java
@@ -1,25 +1,28 @@
 package com.alkemy.wallet.service;
 
-import com.alkemy.wallet.dto.TransactionDto;
+
 import com.alkemy.wallet.dto.request.TransactionRequestDto;
 import com.alkemy.wallet.dto.response.TransactionResponseDto;
 import com.alkemy.wallet.entity.Account;
 import com.alkemy.wallet.entity.Transaction;
 import com.alkemy.wallet.entity.User;
+import com.alkemy.wallet.enums.ERole;
 import com.alkemy.wallet.enums.ETransactionType;
 import com.alkemy.wallet.repository.IAccountRepository;
 import com.alkemy.wallet.repository.ITransactionRepository;
 import com.alkemy.wallet.repository.IUserRepository;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestHeader;
+
+
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
-public class TransactionServiceImpl implements ITransactionService{
+public class TransactionServiceImpl implements ITransactionService {
 
     private final IUserRepository userRepository;
     private final IAccountRepository accountRepository;
@@ -34,42 +37,49 @@ public class TransactionServiceImpl implements ITransactionService{
     }
 
     @Override
-    public TransactionDto getTransaction(Long id,@RequestHeader(name = HttpHeaders.AUTHORIZATION) String token) {
-        //String userEmail = jwtService.extractUserName(token.substring(7));
-        //Optional<User> userOptional = userRepository.findByEmail(userEmail);
-        //if(userOptional.isPresent()){
-            //User user = userOptional.get();
-            Optional<Transaction> transactionOptional = transactionRepository.findById(id);
-            if(transactionOptional.isPresent()){
-                Transaction transaction = transactionOptional.get();
-                TransactionDto transactionDto = new TransactionDto(
-                        null,
-                        transaction.getAccount().getId(),
-                        transaction.getId(),
-                        transaction.getType().name(),
-                        transaction.getAmount(),
-                        transaction.getDescription(),
-                        transaction.getTransactionDate()
-                );
-                return transactionDto;
+    public TransactionResponseDto getTransaction(Long id, String token) {
+        Optional<Transaction> transactionOptional = transactionRepository.findById(id);
+        if (transactionOptional.isPresent()) {
+            Transaction transaction = transactionOptional.get();
+            User transactionOwner = transaction.getAccount().getUser();
+            String userEmail = jwtService.extractUserName(token.substring(7));
+            Optional<User> userOptional = userRepository.findByEmail(userEmail);
+            if (userOptional.isPresent()) {
+                User user = userOptional.get();
+                if (user.getRole().getName() == ERole.ADMIN || Objects.equals(transactionOwner.getId(), user.getId())) {
+                    return new TransactionResponseDto(
+                            transactionOwner.getEmail(),
+                            transaction.getAccount().getId(),
+                            transaction.getId(),
+                            transaction.getAccount().getCurrency().name(),
+                            transaction.getType().name(),
+                            transaction.getAmount(),
+                            transaction.getDescription(),
+                            transaction.getTransactionDate()
+                    );
+                }
             }
-            return null;
+        }
+        return null;
     }
 
+
+
+
     @Override
-    public Boolean createDeposit(TransactionRequestDto depositRequest, String token){
-        if(depositRequest.getAmount() < 0.00){
+    public Boolean createDeposit(TransactionRequestDto depositRequest, String token) {
+        if (depositRequest.getAmount() < 0.00) {
             return null;
         }
         String userEmail = jwtService.extractUserName(token.substring(7));
         Optional<User> userOptional = userRepository.findByEmail(userEmail);
-        if(userOptional.isPresent()){
+        if (userOptional.isPresent()) {
             User user = userOptional.get();
             List<Account> userAccounts = user.getAccounts();
             Optional<Account> accountOptional = userAccounts.stream()
                     .filter(account -> account.getCurrency().name().equals(depositRequest.getCurrency()))
                     .findFirst();
-            if(accountOptional.isPresent()){
+            if (accountOptional.isPresent()) {
                 Account account = accountOptional.get();
                 Transaction newTransaction = new Transaction();
                 newTransaction.setAmount(depositRequest.getAmount());
@@ -89,20 +99,20 @@ public class TransactionServiceImpl implements ITransactionService{
 
     @Override
     public TransactionResponseDto createPayment(TransactionRequestDto paymentRequest, String token) {
-        if(paymentRequest.getAmount() < 0.00){
+        if (paymentRequest.getAmount() < 0.00) {
             return null;
         }
         String userEmail = jwtService.extractUserName(token.substring(7));
         Optional<User> userOptional = userRepository.findByEmail(userEmail);
-        if(userOptional.isPresent()){
+        if (userOptional.isPresent()) {
             User user = userOptional.get();
             List<Account> userAccounts = user.getAccounts();
             Optional<Account> accountOptional = userAccounts.stream()
                     .filter(account -> account.getCurrency().name().equals(paymentRequest.getCurrency()))
                     .findFirst();
-            if(accountOptional.isPresent()){
+            if (accountOptional.isPresent()) {
                 Account account = accountOptional.get();
-                if(account.getBalance() >= paymentRequest.getAmount()){
+                if (account.getBalance() >= paymentRequest.getAmount()) {
                     Transaction newTransaction = new Transaction();
                     newTransaction.setAmount(paymentRequest.getAmount());
                     newTransaction.setType(ETransactionType.DEPOSIT);
@@ -127,4 +137,16 @@ public class TransactionServiceImpl implements ITransactionService{
         }
         return null;
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
