5a06122da345948af3e77e6e7be9b7351446983f
sebastian <sebastiandev.img@gmail.com>
2023-12-08 13:54:40 -0300

    T8S2-169:paginar listado de usuarios

--- src/main/java/com/alkemy/wallet/service/IUserService.java
+++ src/main/java/com/alkemy/wallet/service/IUserService.java
@@ -1,13 +1,14 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.UserDto;
+import com.alkemy.wallet.dto.response.PageableUserResponseDto;
 import com.alkemy.wallet.entity.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
 public interface IUserService {
-    List<UserDto> getUsers();
+    PageableUserResponseDto getUsers(int page);
     User deleteUserById(Long id);
 
     UserDetailsService userDetailsService();
